a99233813bd5b28f73f218eae95133f0b6ce3987
alexicon3000 <alex.number2@gmail.com>
2014-04-25 17:13:44 -0400

    pushing code

---
+++ src/testApp.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxBullet.h"
+#include "ofx3DModelLoader.h"
+
+class testApp : public ofBaseApp{
+
+public:
+	void setup();
+	void update();
+	void draw();
+	
+	void onCollision(ofxBulletCollisionData& cdata);
+	void mousePickEvent( ofxBulletMousePickEvent &e );
+
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
+	ofxBulletWorldRigid			world;
+	vector <ofxBulletBox*>		bounds;
+	float						boundsWidth;
+	
+	ofLight						light;
+	
+	btBoxShape*					boxShape;
+	btSphereShape*				sphereShape;
+    //btCompoundShape*            squirrelShape;
+    vector<ofxBulletBaseShape*>	shapes;
+	vector<bool>				bColliding;
+	int							mousePickIndex;
+	ofVec3f						mousePickPos;
+	
+	bool						bDrawDebug;
+	bool						bRenderShapes;
+	bool						bAddCenterAttract;
+	bool						bSpacebar;
+	
+	ofCamera					camera;
+    
+    ofx3DModelLoader            squirrelModel;
+		
+};
